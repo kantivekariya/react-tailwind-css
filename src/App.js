59f00e6717b0a2af14f8038d42ffde6bd088c902
kantivekariya <kantivekariya242@gmail.com>
2022-08-09 12:34:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+    </div>
+  );
+}
+
+export default App;

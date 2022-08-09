113eeb0453985dd5b5fcd4426b4e29b5ddd6e8c1
kantivekariya <kantivekariya242@gmail.com>
2022-08-09 12:59:11 +0530

    added headline cards

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import HeadlineCards from "./components/HeadlineCard";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Banner />
+      <HeadlineCards />
     </div>
   );
 }
